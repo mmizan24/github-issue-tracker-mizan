d85a0b9a4b4a4668ea026f43a845d79546417384
mizan <mmizan24@gmail.com>
2026-03-07 15:51:48 +0600

    login page done

---
+++ js/login.js
@@ -0,0 +1,12 @@
+function loginUser() {
+
+  const username = document.getElementById('username').value;
+  const password = document.getElementById('password').value;
+
+  if(username === "admin" && password === "admin123"){
+      window.location.href = "main.html";
+  }else{
+      alert("Invalid Credentials");
+  }
+
+}
