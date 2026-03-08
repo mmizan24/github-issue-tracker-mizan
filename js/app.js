5a5035fbcf4562dce469054bcfd43982671d6b96
mizan <mmizan24@gmail.com>
2026-03-08 16:04:43 +0600

    create the modal

--- js/app.js
+++ js/app.js
@@ -1,40 +1,48 @@
 let allIssues = [];
 
-window.onload = async function(){
+window.onload = async function () {
 
-allIssues = await fetchIssues();
+    allIssues = await fetchIssues();
 
-displayIssues(allIssues);
+    displayIssues(allIssues);
 
 }
 
-function displayIssues(issues){
+function displayIssues(issues) {
 
-const container =
-document.getElementById("issueContainer");
+    const container =
+        document.getElementById("issueContainer");
 
-container.innerHTML = "";
+    container.innerHTML = "";
 
-issues.forEach(issue => {
+    issues.forEach(issue => {
 
-let border =
-issue.status === "open"
-? "border-green-500"
-: "border-purple-500";
+        let border =
+            issue.status === "open"
+                ? "border-green-500"
+                : "border-purple-500";
+        
+                let img=
+                issue.status==='open'
+                ? `<img src="./assets/Open-Status.png" alt="Open">`
+                : `<img src="./assets/Closed- Status .png" alt="Closed">`;
 
-const div = document.createElement("div");
+        const div = document.createElement("div");
 
-div.className =
-`border-t-4 ${border} shadow p-4 cursor-pointer rounded-md mt-5 text-left`;
+        div.className =
+            `border-t-4 ${border} shadow p-4 cursor-pointer rounded-md mt-5 text-left`;
 
-div.innerHTML = `
+        div.innerHTML = `
+
+        
 
 <h2 class="font-bold text-wrap">
 ${issue.title}
 </h2>
 <div class="flex justify-between mt-5 mb-5">
- <img src="./assets/Open-Status.png" alt="">
-<button class="bg-yellow-300 rounded-md"> ${issue.priority}</button>
+${img}
+
+<button class="bg-red-300 opacity-50 text-red-600 rounded-md"> ${issue.priority}</button>
 </div>
 <p>${issue.description}</p>
 
@@ -49,73 +57,104 @@ ${issue.title}
 
 `;
 
-div.onclick = () => showModal(issue.id);
+        div.onclick = () => showModal(issue.id);
 
-container.appendChild(div);
+        container.appendChild(div);
 
-});
+    });
 
 }
 
-function filterIssues(type){
 
-let filtered = allIssues;
+function filterIssues(type) {
 
-if(type === "open"){
-filtered = allIssues.filter(
-i => i.status === "open"
-);
-}
+    let filtered = allIssues;
 
-if(type === "closed"){
-filtered = allIssues.filter(
-i => i.status === "closed"
-);
-}
+    if (type === "open") {
+        filtered = allIssues.filter(
+            i => i.status === "open"
+        );
+    }
 
-displayIssues(filtered);
+    if (type === "closed") {
+        filtered = allIssues.filter(
+            i => i.status === "closed"
+        );
+    }
 
-setActiveTab(type);
+    displayIssues(filtered);
+
+    setActiveTab(type);
 
 }
 
 // toggling the btn 
 
-function setActiveTab(type){
+function setActiveTab(type) {
 
-document.querySelectorAll(".tabBtn")
-.forEach(btn => {
-btn.classList.remove("bg-blue-500","text-white");
-});
+    document.querySelectorAll(".tabBtn")
+        .forEach(btn => {
+            btn.classList.remove("bg-blue-500", "text-white");
+        });
 
-document.getElementById(type+"Btn")
-.classList.add("bg-blue-500","text-white");
+    document.getElementById(type + "Btn")
+        .classList.add("bg-blue-500", "text-white");
 
 }
 
 // searching function 
 
-async function searchIssues(){
+async function searchIssues() {
 
-const text =
-document.getElementById("searchInput").value;
+    const text =
+        document.getElementById("searchInput").value;
 
-const result =
-await searchIssueAPI(text);
+    const result =
+        await searchIssueAPI(text);
 
-displayIssues(result);
+    displayIssues(result);
+
+}
+
+// creating modal
+
+async function showModal(id){
+
+const issue =
+await fetchSingleIssue(id);
+
+document.getElementById("modalTitle")
+.innerText = issue.title;
+
+document.getElementById("modalDescription")
+.innerText = issue.description;
+
+document.getElementById("modal")
+.classList.remove("hidden");
+
+}
+
+
+
+function closeModal(){
+
+document.getElementById("modal")
+.classList.add("hidden");
 
 }
 
 // spinnier option 
 
 function showSpinner(){
-document.getElementById("spinner").classList.remove("hidden");
+document.getElementById("spinner")
+.classList.remove("hidden");
 }
 
 function hideSpinner(){
-document.getElementById("spinner").classList.add("hidden");
+document.getElementById("spinner")
+.classList.add("hidden");
 }
+
 
 
 
